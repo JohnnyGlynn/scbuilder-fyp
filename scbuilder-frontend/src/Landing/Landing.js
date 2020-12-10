41983ab9666623463b09b9cdc136567b46468490
Johnny Glynn <johnny@Johnnys-MacBook-Pro.local>
2020-12-10 11:26:44 +0000

    Added dropdown to facilitate contract choice

--- scbuilder-frontend/src/Landing/Landing.js
+++ scbuilder-frontend/src/Landing/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Landing.css';
-import {makeStyles, Button } from "@material-ui/core";
+import {makeStyles, Button, InputLabel, MenuItem, FormControl, Select } from "@material-ui/core";
 
 const styles = makeStyles(() => ({
   menuButton: {
@@ -8,27 +8,75 @@ const styles = makeStyles(() => ({
     fontWeight: 700,
     size: "18px",
     marginLeft: "38px",
- },
+  },
+  formControl: {
+  color: "0000000",
+  margin: "5vh",
+  minWidth: 120,
+  },
 }));
 
 function Landing() {
 
-  const {menuButton} = styles();
+  const {menuButton, formControl} = styles();
+  const [contract, setContract] = React.useState('Contract');
+
+  const handleChange = (event) => {
+    setContract(event.target.value);
+  };
+
+  var contractSelect = () => {
+    return(
+      <div>
+        <FormControl color="secondary" className={formControl}>
+        <InputLabel color="secondary" id="contract-select">Contract Type</InputLabel>
+        <Select
+          labelId="contract-select"
+          id="contract-select"
+          value={contract}
+          onChange={handleChange} 
+          color="secondary"
+        >
+          <MenuItem color="secondary" value={"contract"}>simple contract</MenuItem>
+          <MenuItem color="secondary" value={"subcurrency"}>sub-currency</MenuItem>
+        </Select>
+      </FormControl>
+      </div>
+    );
+  };
 
   function generateSmartContract(){
-    fetch('http://localhost:10000/scgen',{
-      method: 'GET'})
-    .then(response => {
-            console.log(response)
-    })
-    .catch((error) => {
-        console.log("Im an error" + error)
-    });
+
+    const requestOptions = {
+      method: 'POST',
+      // headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify(contract)
+    };
+
+    console.log("Request body" + requestOptions.body)
+
+    fetch('http://localhost:10000/scgen', requestOptions)
+        .then(response => {
+                console.log(response)
+        })
+        .catch((error) => {
+            console.log("Im an error" + error)
+        });
+
+    // fetch('http://localhost:10000/scgen',{
+    //   method: 'GET'})
+    // .then(response => {
+    //         console.log(response)
+    // })
+    // .catch((error) => {
+    //     console.log("Im an error" + error)
+    // });
   }
 
   return (
     <body className = "Landing-body">
       <div>
+        <div>{contractSelect()}</div>
         <Button variant="contained" color="primary" onClick={generateSmartContract} className={menuButton}>Generate Smart-Contract</Button>
       </div>
     </body>
