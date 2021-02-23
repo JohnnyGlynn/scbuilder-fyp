2ef56803b3b71bcc3f406674989d8f4d47373ab5
JohnnyGlynn <idyslexiahave@gmail.com>
2021-02-23 23:27:48 +0000

    Added test transaction and Go-ethereum bindings

---
+++ scbuilder-backend/cmd/transaction_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum"
+
+)
+
+func test_transaction(){
+	
+}
--- scbuilder-backend/go.mod
+++ scbuilder-backend/go.mod
@@ -1,3 +1,5 @@
 module scbuilder-backend
 
 go 1.15
+
+require github.com/ethereum/go-ethereum v1.9.25
