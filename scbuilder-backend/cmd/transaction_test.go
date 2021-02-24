f1a4038aa83435ca63da1200f65311e44985051a
JohnnyGlynn <idyslexiahave@gmail.com>
2021-02-24 00:06:48 +0000

    scaffolding for tx

--- scbuilder-backend/cmd/transaction_test.go
+++ scbuilder-backend/cmd/transaction_test.go
@@ -6,11 +6,35 @@ import (
 	"fmt"
 	"log"
 	"math/big"
+	"os"
 
-	"github.com/ethereum/go-ethereum"
-
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func test_transaction(){
+
+	/* Define a transaction
+	* Create client for Ropsten Testnet
+	* Get private key, and generate public key from that
+	* Initialte transaction FROM public address
+	* Define Value and Gas price, use suggested
+	* Define Target address for Tx
+	* Send Tx, return the transaction hash to allow users to look at transaction
+	*/
+
+	client, err := ethclient.Dial("https://ropsten.infura.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKey, err := crypto.HexToECDSA(os.Getenv("TESTNET_BACKEND_WALLET_PRIVATE_KEY"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
 	
 }
