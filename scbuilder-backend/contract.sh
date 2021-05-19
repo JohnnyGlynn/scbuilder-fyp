9dfc0b392b1f12a0d9044c1f9bda570bed27284c
JohnnyGlynn <idyslexiahave@gmail.com>
2021-05-19 01:33:33 +0100

    typo

--- scbuilder-backend/contract.sh
+++ scbuilder-backend/contract.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /tmp/contracts > /dev/null
-cp ~/trufflestuff/truffle-config.js . && cp ~/trufflestuff/package.json . && cp ~/trufflestuff/package-lock.json . && cp -R ~/trufflestuff/node_modules ./node_modules > /dev/null
+cp ../truffle-config/truffle-config.js . && cp ../truffle-config/package.json . && cp ../truffle-config/package-lock.json . && cp -R ../truffle-config/node_modules ./node_modules > /dev/null
 echo -n | truffle init > /dev/null
 truffle compile > /dev/null
 truffle deploy --network ropsten-infura --reset | sed -n 's/.*> transaction hash:    //p'
